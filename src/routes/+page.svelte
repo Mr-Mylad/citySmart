1a369044d2a9262861d0dcaaa925a125e0bba8a6
Mr. Mylad <mrmylad10@gmail.com>
2024-12-28 15:47:35 +0530

    Finished interactive map

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,32 @@
-<!-- <script>
-    import { GoogleGenerativeAI } from "@google/generative-ai";
+<script>
+    // General stuff
+    import { onMount } from "svelte";
     import { marked } from "marked";
-    import { initialAnalysis } from "$lib"
+    import { goto } from "$app/navigation"
+    // AI stuff
+    import { GoogleGenerativeAI } from "@google/generative-ai";    
     const genAI = new GoogleGenerativeAI(import.meta.env.GEMINI_API_KEY);
     const client = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-    let initialAnaTest = " ";
+    // Extra functions
+    import { initialAnalysis } from "$lib"
+    // Components
+    import Map from "$lib/components/Map.svelte";
     
-</script> -->
+    function nextPage() {
+        const locationInput = document.getElementsByClassName("mapboxgl-ctrl-geocoder--input")[0];
+        if (locationInput) {
+            const location = locationInput.value;
+            localStorage.setItem("location", location);
+            goto("/chat")
+        }
+    }
+</script>
 
-<body class="bg-black">
-</body>
-
-<!-- {@html marked(initialAnaTest)} -->
+<h1 class="font-bold text-4xl text-white">CitySmart - THE Smart City Planner</h1>
+<br>
+<Map></Map>
+<div id="buttons" class="fixed bottom-10 left-0">
+    <p class="text-sm text-white ">Search the place you want to know about and press the button!</p>
+    <br>
+    <button class=" border-4 border-black rounded-lg bg-white p-1" on:click={nextPage}>Continue</button>
+</div>
